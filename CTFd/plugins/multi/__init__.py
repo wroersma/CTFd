598ce5d51021a365474141f13095fd50cf13ddc0
Wyatt Roersma <wyatt.roersma@target.com>
2017-10-02 18:24:16 -0600

    remove duplicate code

--- CTFd/plugins/multi/__init__.py
+++ CTFd/plugins/multi/__init__.py
@@ -2,13 +2,14 @@ from CTFd.plugins.keys import get_key_class
 from CTFd.plugins import challenges
 from CTFd.plugins import keys
 from CTFd.models import db, Keys, WrongKeys
+from CTFd.plugins.keys import BaseKey
 
 
 class MultiChallenge(challenges.BaseChallenge):
+    """Multi-Challenge allows right and wrong answers and leaves the question open"""
     id = 2
     name = "multi-challenge"
 
-    @staticmethod
     def attempt(chal, request):
         provided_key = request.form['key'].strip()
         chal_keys = Keys.query.filter_by(chal=chal.id).all()
@@ -20,7 +21,6 @@ class MultiChallenge(challenges.BaseChallenge):
                     return False, 'Incorrect'
         return False, 'Incorrect'
 
-    @staticmethod
     def solve(team, chal, request):
         provided_key = request.form['key'].strip()
         solve = Solves(teamid=team.id, chalid=chal.id, ip=utils.get_ip(req=request), flag=provided_key)
@@ -28,7 +28,6 @@ class MultiChallenge(challenges.BaseChallenge):
         db.session.commit()
         db.session.close()
 
-    @staticmethod
     def fail(team, chal, request):
         provided_key = request.form['key'].strip()
         wrong = WrongKeys(teamid=team.id, chalid=chal.id, ip=utils.get_ip(request), flag=provided_key)
@@ -36,14 +35,14 @@ class MultiChallenge(challenges.BaseChallenge):
         db.session.commit()
         db.session.close()
 
+    def solve(chal, provided_key):
+        chal_keys = Keys.query.filter_by(chal=chal.id).all()
+        for chal_key in chal_keys:
+            if get_key_class(chal_key.key_type).compare(chal_key.flag, provided_key):
+                return True
+        return False
 
-class BaseKey(object):
-    id = None
-    name = None
 
-    @staticmethod
-    def compare(self, saved, provided):
-        return True
 
 
 class CTFdWrongKey(BaseKey):
@@ -58,6 +57,13 @@ class CTFdWrongKey(BaseKey):
         for x, y in zip(saved, provided):
             result |= ord(x) ^ ord(y)
         return result == 0
+
+
+def get_chal_class(class_id):
+    cls = challenges.CHALLENGE_CLASSES.get(class_id)
+    if cls is None:
+        raise KeyError
+    return cls
 
 
 def load(app):
